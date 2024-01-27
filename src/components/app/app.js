b38e83c7c851e80ad2fe78645320d874675a55f7
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-01-27 17:37:22 +0500

    change component App to class component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,33 +5,34 @@ import SearchPanel from "../search-panel/search-panel"
 import AppFilter from "../app-filter/app-filter"
 import MovieList from "../movie-list/movie-list"
 import MoviesAddForm from "../movies-add-form/movies-add-form"
+import { Component } from "react"
 
-const App = () => {
-
-
-  const data = [
+class App extends Component {
+  data = [
     {id: 1, name: "Empire of osman", viewers: 811, favourite: false},
     {id: 2, name: "Ertugrul", viewers: 890, favourite: true},
     {id: 3, name: "Omar", viewers: 999, favourite: false},
   ]
 
-  const onDelete = id => {
+  onDelete = id => {
     console.log(id)
   }
 
-  return (
-    <div className="app font-monospace">
-      <div className="content">
-        <AppInfo/>
-        <div className="search-panel" >
-          <SearchPanel />
-          <AppFilter />
+  render() {
+    return (
+      <div className="app font-monospace">
+        <div className="content">
+          <AppInfo/>
+          <div className="search-panel" >
+            <SearchPanel />
+            <AppFilter />
+          </div>
+          <MovieList data={this.data} onDelete={this.onDelete} />
+          <MoviesAddForm />
         </div>
-        <MovieList data={data} onDelete={onDelete} />
-        <MoviesAddForm />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -5,7 +5,7 @@ class MoviesAddForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "+998-(90)-",
+      name: "",
       viewers: ""
     };
   }
