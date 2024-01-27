ef83a0440125a52e5d907ca5aea13279d925e58d
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-01-27 18:13:27 +0500

    transform movie-list to class component

--- src/components/movie-list/movie-list.js
+++ src/components/movie-list/movie-list.js
@@ -1,15 +1,30 @@
 import "./movie-list.css"
 
+import { Component } from "react"
 import MovieListItem from "../movie-list-item/movie-list-item"
 
-const MovieList = ({data, onDelete}) => {
-  return (
-    <ul className='movie-list'>
-      {data?.map((e) => (
-        <MovieListItem {...e} key={e.id} onDelete={() => onDelete(e.id)} />
-      ))}
-    </ul>
-  )
+class MovieList extends Component {
+  render() {
+    const {data, onDelete} = this.props
+    return (
+      <ul className='movie-list'>
+        {data?.map((e) => (
+          <MovieListItem {...e} key={e.id} onDelete={() => onDelete(e.id)} />
+        ))}
+      </ul>
+    )
+  }
+
 }
+
+// const MovieList = ({data, onDelete}) => {
+//   return (
+//     <ul className='movie-list'>
+//       {data?.map((e) => (
+//         <MovieListItem {...e} key={e.id} onDelete={() => onDelete(e.id)} />
+//       ))}
+//     </ul>
+//   )
+// }
 
 export default MovieList
