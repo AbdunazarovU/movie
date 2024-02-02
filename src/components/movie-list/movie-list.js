fd8f6fb13dbcc161e64b9916fec8f33eab7ae2e7
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-02 15:22:45 +0500

    convert MovieList to functional component

--- src/components/movie-list/movie-list.js
+++ src/components/movie-list/movie-list.js
@@ -1,34 +1,22 @@
 import "./movie-list.css";
 
-import { Component } from "react";
 import MovieListItem from "../movie-list-item/movie-list-item";
 
-class MovieList extends Component {
-  render() {
-    const { data, onDelete, onToggleProp } = this.props;
-    return (
-      <ul className="movie-list">
-        {data?.map((e) => (
-          <MovieListItem
-            key={e.id}
-            {...e}
-            onDelete={() => onDelete(e.id)}
-            onToggleProp={el => onToggleProp(e.id, el.currentTarget.getAttribute("data-toggle"))}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
-
-// const MovieList = ({data, onDelete}) => {
-//   return (
-//     <ul className='movie-list'>
-//       {data?.map((e) => (
-//         <MovieListItem {...e} key={e.id} onDelete={() => onDelete(e.id)} />
-//       ))}
-//     </ul>
-//   )
-// }
+const MovieList = ({ data, onDelete, onToggleProp }) => {
+  return (
+    <ul className="movie-list">
+      {data?.map((e) => (
+        <MovieListItem
+          {...e}
+          key={e.id}
+          onDelete={() => onDelete(e.id)}
+          onToggleProp={(el) =>
+            onToggleProp(e.id, el.currentTarget.getAttribute("data-toggle"))
+          }
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default MovieList;
